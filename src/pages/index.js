fb69d3f4edc87bf68330addce2d1c60fd7fb8a0b
Vidhyadharan <it.vidhyadharan@gmail.com>
2018-03-12 23:12:39 +0530

    Added 3rd page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,8 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <br/>
+    <Link to="/page-3/">Go to page 3</Link>
   </div>
 )
 
