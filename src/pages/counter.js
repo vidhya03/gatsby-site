294020b5c2db03b0b4a7d195f69f5d90431b8361
Vidhyadharan <it.vidhyadharan@gmail.com>
2018-03-15 00:09:04 +0530

    added custom attribute

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -4,12 +4,14 @@ class Counter extends React.Component {
   constructor() {
     super()
     this.state = { count: 0 }
+    this.custom = { count: 10 }
   }
   render() {
     return <div>
             <h1>Counter</h1>
             <p>current count: {this.state.count}</p>
-            <button onClick={() => this.setState({ count: this.state.count - 5 })}>-5</button>
+            <p>current custom: {this.custom.count}</p>           
+            <button onClick={() => this.setCustom({ count: this.custom.count + 1 })}>Custom ++</button>
             <button onClick={() => this.setState({ count: this.state.count + 1 })}>plus</button>
             <button onClick={() => this.setState({ count: this.state.count - 1 })}>minus</button>
             <button onClick={() => this.setState({ count: this.state.count + 5 })}>+5</button>
