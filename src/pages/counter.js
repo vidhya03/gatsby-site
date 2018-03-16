aa4a197d2102f12a594ba0d1386ebe00b8031178
Vidhyadharan <it.vidhyadharan@gmail.com>
2018-03-16 22:30:30 +0530

    custom state

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -3,15 +3,14 @@ import React from 'react'
 class Counter extends React.Component {
   constructor() {
     super()
-    this.state = { count: 0 }
-    this.custom = { count: 10 }
+    this.state = { count: 0, count01: 12 }
   }
   render() {
     return <div>
             <h1>Counter</h1>
             <p>current count: {this.state.count}</p>
-            <p>current custom: {this.custom.count}</p>           
-            <button onClick={() => this.setCustom({ count: this.custom.count + 1 })}>Custom ++</button>
+            <p>current count01: {this.state.count01}</p>           
+            <button onClick={() => this.setState({ count01 : this.state.count01 + 1 })}>Custom ++</button>
             <button onClick={() => this.setState({ count: this.state.count + 1 })}>plus</button>
             <button onClick={() => this.setState({ count: this.state.count - 1 })}>minus</button>
             <button onClick={() => this.setState({ count: this.state.count + 5 })}>+5</button>
